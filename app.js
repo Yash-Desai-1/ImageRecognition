a8c34d7bace92e3f80d9a84f161da9c9db3afd1c
Yash-Desai-1 <yashdesai4320@gmail.com>
2022-09-06 09:26:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config({path:"./configs/.env"});
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const http = require('http');
+const predictRouter = require('./routes/predict');
+
+const app = express();
+const cors = require("cors")
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/hello', function (req, res) {
+    res.send("hello")
+})
+;
+app.use(predictRouter); 
+// app.get('/', function (req, res) {
+//     return res.sendFile(path.join(__dirname, "build", "index.html"))
+// })
+const port = process.env.PORT || '8080';
+
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', () => {
+    console.log('Listening on ' + (port));
+});
